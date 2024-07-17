4e40cbb12a088805e2a7cf3a2bad120f8d0469e2
Loris Beltramo <loris.beltramo@dncsrl.com>
2024-07-17 14:58:35 +0200

    fix code

--- src/DataBase/DbConnection.js
+++ src/DataBase/DbConnection.js
@@ -1,11 +1,8 @@
 import { Db, MongoClient } from 'mongodb';
 
-//const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}.enuharq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&authMechanism=DEFAULT`;
 const url = `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/?${process.env.MONGO_PARAMS}`;
 
-//const url = process.env.MONGO_URL;
-export const client = new MongoClient(url//, { useNewUrlParser: true, useUnifiedTopology: true }
-);
+export const client = new MongoClient(url);
 //const dbName = `e-commerce_Ivan`;
 const dbName = process.env.MONGO_DBNAME;
 /**
