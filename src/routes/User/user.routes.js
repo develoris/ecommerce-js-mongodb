d8723e4025926d777a0a08f1bde0e8ad4f98544b
Ivan Tortoriello <ivan.tortoriello@dncsrl.com>
2024-07-23 09:45:00 +0200

    user validation and password problems

--- src/routes/User/user.routes.js
+++ src/routes/User/user.routes.js
@@ -2,19 +2,19 @@ import { Router } from 'express';
 import * as controller from './user.controller.js';
 import verifyToken from '../../middleware/authMiddleware.js';
 import { validate } from 'express-validation';
-import { createUserValidation, loginUserValidation, updateUserValidation, idValidation } from './user.validation.js';
+import * as V from './user.validation.js';
 
 
 const userRouter = Router();
 
 userRouter.get('/',controller.getAll);
-userRouter.post('/', validate(createUserValidation, {}, {}), controller.create);
-userRouter.post('/login', validate(loginUserValidation, {}, {}), controller.loginUser);
+userRouter.post('/', validate(V.register, {}, {}), controller.create);
+userRouter.post('/login', validate(V.login, {}, {}), controller.loginUser);
 userRouter.delete('/logout', verifyToken, controller.deleteToken);
 userRouter.get('/me', verifyToken, controller.getMe);
 userRouter.post('/refresh',  controller.refreshToken);
-userRouter.put('/:id',verifyToken, validate(updateUserValidation, {}, {}), controller.updateUser);
-userRouter.get('/:id', validate(idValidation, {}, {}), controller.getbyId);
+userRouter.put('/:id',verifyToken, validate(V.updateUser, {}, {}), controller.updateUser);
+userRouter.get('/:id', validate(V.idValidation, {}, {}), controller.getbyId);
 
 export default userRouter;
  
